751f86563115a6633c74025804482e40b3a162f4
Zeynep Bayraktar <zeynepbayraktar2006@gmail.com>
2026-07-08 17:26:17 +0300

    Add RdRp extraction pipeline for 8 mycoviruses + FASTA output

--- mycovirus_analysis2.py
+++ mycovirus_analysis2.py
@@ -71,3 +71,64 @@ print("Benzerlik skoru:", score)
 max_score = min(len(rdp1), len(rdp2))
 similarity = (score / max_score) * 100
 print(f"Normalize benzerlik: {similarity:.2f}%")
+
+print("\n--- NCBI SEQUENCE'LERİ ---")
+handle = Entrez.esearch(db="nucleotide", term="Sahin E[Author] mycovirus", retmax=50)
+search_record = Entrez.read(handle)
+handle.close()
+
+print("Toplam sonuç:", search_record["Count"])
+print("ID'ler:", search_record["IdList"])
+
+handle = Entrez.efetch(db="nucleotide", id=search_record["IdList"], rettype="acc", retmode="text")
+print(handle.read())
+handle.close()
+
+handle = Entrez.efetch(db="nucleotide", id=search_record["IdList"], rettype="gb", retmode="text")
+records = list(SeqIO.parse(handle, "genbank"))
+handle.close()
+
+for r in records:
+    print(r.id, "→", r.description)
+
+all_accessions = [
+    "PP389084.1",
+    "PP389085.1", 
+    "NC_116531.1",
+    "MT764202.1",
+    "MT764195.1",
+    "MT764194.1",
+    "MT876191.1",
+    "MT876190.1"
+]
+
+rdp_sequences = {}
+
+for accession in all_accessions:
+    print(accession)
+    handle = Entrez.efetch(db="nucleotide", id=accession, rettype="gb", retmode="text")
+    record = SeqIO.read(handle, "genbank")
+    handle.close()
+    for feature in record.features:
+        if feature.type == "CDS":
+            protein_id = feature.qualifiers["protein_id"][0]
+    handle = Entrez.efetch(db="protein", id=protein_id, rettype="gb", retmode="text")
+    record_protein= SeqIO.read(handle, "genbank")
+    handle.close()
+    for feature in record_protein.features:
+        if feature.type == "Region":
+            if "region_name" in feature.qualifiers:
+                if "RdRp" in feature.qualifiers["region_name"][0]:
+                   print("RdRp bulundu:", feature.location)
+    start = feature.location.start
+    end = feature.location.end
+    rdp_seq = record_protein.seq[start:end]
+    print("RdRp sequence:", rdp_seq[:30])
+    rdp_sequences[accession] = str(rdp_seq)
+
+print(rdp_sequences)
+
+with open("rdp_sequences.fasta", "w") as f:
+    for accession, seq in rdp_sequences.items():
+        f.write(f">{accession}\n{seq}\n")
+
